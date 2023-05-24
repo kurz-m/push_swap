e30554529d668b30c91fd2473eb2a859a4df8c0f
kurz-m <m.kurz92@gmail.com>
2023-05-24 07:29:01 +0200

    create struct

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,11 +6,19 @@
 /*   By: makurz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 16:28:54 by makurz            #+#    #+#             */
-/*   Updated: 2023/05/23 16:29:20 by makurz           ###   ########.fr       */
+/*   Updated: 2023/05/23 19:47:26 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+typedef struct s_circle
+{
+	int					number;
+	struct s_circle		*next;
+	struct s_circle		*previous;
+	int					index;
+}	t_circle;
 
 #endif
