21f5cf2df5e373cbd055f80d588799b77c3d41f7
kurz-m <m.kurz92@gmail.com>
2023-05-29 11:51:03 +0200

    small syntax change

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: makurz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 16:28:54 by makurz            #+#    #+#             */
-/*   Updated: 2023/05/23 19:47:26 by makurz           ###   ########.fr       */
+/*   Updated: 2023/05/25 21:34:12 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 typedef struct s_circle
 {
 	int					number;
+	int					index;
 	struct s_circle		*next;
 	struct s_circle		*previous;
-	int					index;
 }	t_circle;
 
 #endif
