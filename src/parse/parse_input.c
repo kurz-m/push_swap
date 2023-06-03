a2e9d1b7d5f92215156f77ce07c8a369639d0c28
Markus Kurz <makurz@1-B-29.42heilbronn.de>
2023-06-03 15:45:57 +0200

    start with parsing

--- src/parse/parse_input.c
+++ src/parse/parse_input.c
@@ -1,30 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   parse_input.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: makurz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/23 16:28:54 by makurz            #+#    #+#             */
-/*   Updated: 2023/06/03 13:41:01 by makurz           ###   ########.fr       */
+/*   Created: 2023/06/03 13:30:00 by makurz            #+#    #+#             */
+/*   Updated: 2023/06/03 13:40:31 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "libft.h"
+#include "push_swap.h"
+#include "utils.h"
 
-# include <stdlib.h>
-# include "utils.h"
-
-// Defines for error handling
-# define USAGE 42
-
-typedef struct s_container
+t_circle	**parse_input(int argc, char **argv)
 {
-	t_circle	**stack_a;
-	t_circle	**stack_b;
-}	t_container;
+	t_circle	**stack;
 
-t_circle	**parse_input(int argc, char **argv);
-
-#endif
+	return (stack);
+}
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: makurz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 14:28:45 by makurz            #+#    #+#             */
-/*   Updated: 2023/06/02 16:39:12 by makurz           ###   ########.fr       */
+/*   Updated: 2023/06/03 13:20:40 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@
 
 int	main(int argc, char **argv)
 {
+	t_container		*stack;
+
 	if (argc < 2)
 		return (1);
+	stack->stack_a = parse_input(argc, argv);
 	return (EXIT_SUCCESS);
 }
