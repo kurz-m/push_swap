ad35ec4c63f4b62fda5bbcebd0991e3afffd76be
kurz-m <m.kurz92@gmail.com>
2023-06-04 20:10:34 +0200

    preindexing of values

--- src/utils/init.c
+++ src/utils/init.c
@@ -6,7 +6,7 @@
 /*   By: work <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 15:12:33 by work              #+#    #+#             */
-/*   Updated: 2023/06/04 16:28:58 by work             ###   ########.fr       */
+/*   Updated: 2023/06/04 20:09:55 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ void	construct(t_oop *self)
 {
 	self->top = NULL;
 	self->deconstructor = desconstruct;
+	self->indexing = indexing;
 	self->pop = pop;
 	self->append = append;
 	self->prepend = prepend;
@@ -45,6 +46,29 @@ void	desconstruct(t_oop *self)
 		tmp = current;
 		current = current->next;
 		free(tmp);
+		if (current == self->top)
+			break ;
+	}
+}
+
+void	indexing(t_oop *self)
+{
+	t_circle	*current;
+	t_circle	*run;
+
+	current = self->top;
+	while (1)
+	{
+		run = self->top;
+		while (1)
+		{
+			if (current->data > run->data)
+				current->index += 1;
+			run = run->next;
+			if (run == self->top)
+				break ;
+		}
+		current = current->next;
 		if (current == self->top)
 			break ;
 	}
