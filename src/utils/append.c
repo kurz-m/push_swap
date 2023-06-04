bad4f9f18bc8a3344151fe3df261dd9877db5209
kurz-m <m.kurz92@gmail.com>
2023-06-04 03:40:02 +0200

    start oop

--- src/utils/append.c
+++ src/utils/append.c
@@ -6,7 +6,7 @@
 /*   By: work <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 14:02:40 by work              #+#    #+#             */
-/*   Updated: 2023/06/02 08:44:17 by work             ###   ########.fr       */
+/*   Updated: 2023/06/03 21:34:06 by work             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,25 @@
 #include "libft.h"
 #include "push_swap.h"
 
-void	append(t_circle **head, t_circle *new_node)
+// void	append(t_circle **head, t_circle *new_node)
+// {
+// 	t_circle	*tmp;
+// 
+// 	if (head == NULL)
+// 	{
+// 		*head = new_node;
+// 		(*head)->next = new_node;
+// 		(*head)->previous = new_node;
+// 		return ;
+// 	}
+// 	tmp = (*head)->previous;
+// 	(*head)->previous = new_node;
+// 	tmp->next = new_node;
+// 	new_node->previous = tmp;
+// 	new_node->next = *head;
+// }
+
+void	append(t_oop *self, t_circle *new_node)
 {
 	t_circle	*tmp;
 
