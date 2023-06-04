c910ea0d5e9900cd0af6ddf6cbbbfc23a6e6c47f
kurz-m <m.kurz92@gmail.com>
2023-06-04 03:40:02 +0200

    start oop

--- include/utils.h
+++ include/utils.h
@@ -6,12 +6,12 @@
 /*   By: work <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 16:14:19 by work              #+#    #+#             */
-/*   Updated: 2023/06/02 09:12:17 by work             ###   ########.fr       */
+/*   Updated: 2023/06/04 03:39:36 by work             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef STACK_H
-# define STACK_H
+#ifndef UTILS_H
+# define UTILS_H
 
 typedef struct s_circle
 {
@@ -21,6 +21,17 @@ typedef struct s_circle
 	struct s_circle		*previous;
 }	t_circle;
 
+typedef struct s_oop
+{
+	t_circle	*top;
+	void		(*constructor)(struct s_oop *, t_circle *nwq_node);
+	void		(*rotate)(struct s_oop *, t_circle *new_node);
+	void		(*revrotate)(struct s_oop *, t_circle *new_node);
+	void		(*append)(struct s_oop *, t_circle *new_node);
+	void		(*prepend)(struct s_oop *, t_circle *new_node);
+}	t_oop;
+
+// void		append(t_circle **head, t_circle *new_node);
 void		append(t_circle **head, t_circle *new_node);
 t_circle	*new_node(int data);
 void		prepend(t_circle **head, t_circle *new_node);
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: makurz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 14:28:45 by makurz            #+#    #+#             */
-/*   Updated: 2023/06/03 13:20:40 by makurz           ###   ########.fr       */
+/*   Updated: 2023/06/03 21:33:07 by work             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,14 @@
 
 int	main(int argc, char **argv)
 {
-	t_container		*stack;
+	// t_container		*stack;
+	t_oop			stack;
+
+	stack.append = append;
+	(void) argv;
 
 	if (argc < 2)
 		return (1);
-	stack->stack_a = parse_input(argc, argv);
+	// stack->stack_a = parse_input(argc, argv);
 	return (EXIT_SUCCESS);
 }
