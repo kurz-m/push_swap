73eb08c46096659dd259bb37a384de327493096c
kurz-m <m.kurz92@gmail.com>
2023-06-06 11:58:47 +0200

    working parsing

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: makurz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 16:28:54 by makurz            #+#    #+#             */
-/*   Updated: 2023/06/05 22:59:22 by makurz           ###   ########.fr       */
+/*   Updated: 2023/06/06 11:22:34 by work             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,6 @@ typedef struct s_container
 
 int		parse_input(t_oop **stack, int argc, char **argv);
 void	error_handling(t_oop **stack, int error_code);
+void	parse_error(t_oop **stack, char **nbs);
 
 #endif
--- src/error_handling.c
+++ src/error_handling.c
@@ -6,7 +6,7 @@
 /*   By: makurz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 16:25:31 by makurz            #+#    #+#             */
-/*   Updated: 2023/06/05 16:15:28 by makurz           ###   ########.fr       */
+/*   Updated: 2023/06/06 11:22:48 by work             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,15 @@ void	error_handling(t_oop **stack, int error_code)
 	if (error_code == PARSE)
 	{
 		(*stack)->deconstructor(stack);
+		free((*stack));
+		*stack = NULL;
 		write(2, "Error\n", 6);
 		exit(EXIT_FAILURE);
 	}
+}
+
+void	parse_error(t_oop **stack, char **nbs)
+{
+	ft_arrfree(nbs);
+	error_handling(stack, PARSE);
 }
